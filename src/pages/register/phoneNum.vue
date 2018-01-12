41cde5fbbd250903be85a167f1ce2d34f005b19c
ISolddriver <yhex7111584@163.com>
2018-01-12 14:36:36 +0800

    2018-01-12-joviCommit

---
+++ src/pages/register/phoneNum.vue
@@ -0,0 +1,90 @@
+<template>
+	<div>
+		<div class="inputItem">
+				<span class="inputDes">手机号 (+86)</span>
+				<i class="numRight iconfont" v-show="right">&#xe650;</i>
+				<i class="numError iconfont" v-show="error">&#xe652;</i>
+				<input type="text" class="des" @blur="handleNumberInput">
+		</div>	
+	</div>
+</template>
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'phone-num',
+    data () {
+      return {
+        phoneNum: 0,
+        phoneReg: false,
+        phoneInfo: false,
+        right: false,
+        error: false
+      }
+    },
+    methods: {
+      handleNumberInput (e) {
+        this.phoneNum = e.target.value
+        const reg = /^1[3|4|5|8|7][0-9]\d{4,8}$/
+        reg.test(this.phoneNum) ? this.phoneReg = true : this.phoneReg = false
+        if (this.phoneReg) {
+          this.right = true
+          this.error = false
+          this.getRegisterInfo(this.phoneNum)
+        } else {
+          this.error = true
+          this.right = false
+        }
+      },
+      getRegisterInfo (code) {
+        var codeNum = code
+        axios.get('/api/register.json?' + codeNum)
+         .then(this.handleSucc.bind(this))
+         .catch(this.handleError.bind(this))
+      },
+      handleSucc (res) {
+        this.phoneInfo = res.data.ret
+        this.$emit('phoneNumConfirm', this.phoneInfo)
+      },
+      handleError () {
+        this.phoneInfo = false
+        console.log('error')
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus">
+		.inputItem
+			position: relative
+			height: .88rem
+			width: 100%
+			margin: .3rem 0
+			color: #fe6869
+			background: #fff
+			.inputDes
+				display: block
+				width: 30%
+				float: left
+				text-align: center
+				line-height: .88rem
+			.des
+				display: block
+				width: 40%
+				height: .88rem
+				float: left
+				border: none
+			.verificationCode
+				line-height: .88rem
+			.numRight
+				position: absolute
+				top: .32rem
+				right: 2rem
+				font-size: .32rem
+				font-weight: 900
+				color: green
+			.numError
+				position: absolute
+				top: .32rem
+				right: 2rem
+				font-size: .32rem
+				font-weight: 900
+</style>
